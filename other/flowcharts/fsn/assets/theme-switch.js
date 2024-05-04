d6b35153092d1d9e3e2b4e0d3c70ac9e532148bf
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2024-05-04 13:59:49 +0200

    flowcharts

--- other/flowcharts/fsn/assets/theme-switch.js
+++ other/flowcharts/fsn/assets/theme-switch.js
@@ -5,20 +5,38 @@
 
 // !!! requires fontawesome 5/6 !!!
 
-document.addEventListener('DOMContentLoaded', () => {
-    const lightIconElementId = 'light-toggled';
-    const darkIconElementId = 'dark-toggled';
-    const darkmodeToggleContainerElementId = 'darkmode-toggle-container';
-    const darkmodeToggleElementId = 'darkmode-toggle';
-    const lightModeName = 'light';
-    const darkModeName = 'dark';
-    const darkModeThemeStorageKey = 'dark-mode';
+const lightIconElementId = 'light-toggled';
+const darkIconElementId = 'dark-toggled';
+const darkmodeToggleContainerElementId = 'darkmode-toggle-container';
+const darkmodeToggleElementId = 'darkmode-toggle';
+const lightModeName = 'light';
+const darkModeName = 'dark';
+const darkModeThemeStorageKey = 'dark-mode';
 
-    {
-        // Create a style element
-        const styleElement = document.createElement('style');
-        // Define the CSS rules
-        const cssRules = `
+// Set the dark mode preference
+function setDarkMode(isDarkMode) {
+	document.body.dataset.bsTheme = isDarkMode ? darkModeName : lightModeName; // Bootstrap 5 ligh dark mode switch
+	localStorage.setItem(darkModeThemeStorageKey, isDarkMode); // Store the dark mode preference in local storage
+	if (typeof changeDarkMode === 'function') changeDarkMode(isDarkMode); // Call the changeDarkMode function if it exists
+}
+
+// Check if the dark mode preference is set to true
+function isDarkMode() {
+	if (localStorage.getItem(darkModeThemeStorageKey)) return localStorage.getItem(darkModeThemeStorageKey) == 'true'; // Return the dark mode preference from local storage
+	return (window.matchMedia && window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).matches); // Check if the user prefers dark mode
+}
+
+// Toggle the dark mode preference
+function toggleDarkMode() {
+	setDarkMode(!isDarkMode());
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	{
+		// Create a style element
+		const styleElement = document.createElement('style');
+		// Define the CSS rules
+		const cssRules = `
 			html {
 				color-scheme: light dark;
 			}
@@ -45,64 +63,46 @@ document.addEventListener('DOMContentLoaded', () => {
                 text-shadow: 0 0 3px #FFE8E8, 0 0 3px #000000ff;
             }
 		`;
-        // Set the CSS rules for the style element
-        styleElement.innerHTML = cssRules;
-        // Append the style element to the head of the document
-        document.head.appendChild(styleElement);
-    }
+		// Set the CSS rules for the style element
+		styleElement.innerHTML = cssRules;
+		// Append the style element to the head of the document
+		document.head.appendChild(styleElement);
+	}
 
-    {
-        // Create container element
-        const containerElement = document.createElement('div');
-        containerElement.id = darkmodeToggleContainerElementId;
-        // Create toggle element
-        const toggleElement = document.createElement('div');
-        toggleElement.id = darkmodeToggleElementId;
-        // Create light icon element
-        const lightIconElement = document.createElement('div');
-        lightIconElement.id = lightIconElementId;
-        lightIconElement.className = 'fa-solid fa-sun';
-        // Create dark icon element
-        const darkIconElement = document.createElement('div');
-        darkIconElement.id = darkIconElementId;
-        darkIconElement.className = 'fa-regular fa-moon';
-        // Append elements to form the structure
-        toggleElement.appendChild(lightIconElement);
-        toggleElement.appendChild(darkIconElement);
-        containerElement.appendChild(toggleElement);
-        // Append the container element to the body
-        document.body.appendChild(containerElement);
-    }
+	{
+		// Create container element
+		const containerElement = document.createElement('div');
+		containerElement.id = darkmodeToggleContainerElementId;
+		// Create toggle element
+		const toggleElement = document.createElement('div');
+		toggleElement.id = darkmodeToggleElementId;
+		// Create light icon element
+		const lightIconElement = document.createElement('div');
+		lightIconElement.id = lightIconElementId;
+		lightIconElement.className = 'fa-solid fa-sun';
+		// Create dark icon element
+		const darkIconElement = document.createElement('div');
+		darkIconElement.id = darkIconElementId;
+		darkIconElement.className = 'fa-regular fa-moon';
+		// Append elements to form the structure
+		toggleElement.appendChild(lightIconElement);
+		toggleElement.appendChild(darkIconElement);
+		containerElement.appendChild(toggleElement);
+		// Append the container element to the body
+		document.body.appendChild(containerElement);
+	}
 
-    // Set the dark mode preference
-    function setDarkMode(isDarkMode) {
-        document.body.dataset.bsTheme = isDarkMode ? darkModeName : lightModeName; // Bootstrap 5 ligh dark mode switch
-        localStorage.setItem(darkModeThemeStorageKey, isDarkMode); // Store the dark mode preference in local storage
-        if (typeof changeDarkMode === 'function') changeDarkMode(isDarkMode); // Call the changeDarkMode function if it exists
-    }
+	// Listen for clicks on the toggle
+	document.getElementById(darkmodeToggleElementId).addEventListener('click', () => toggleDarkMode());
 
-    // Check if the dark mode preference is set to true
-    function isDarkMode() {
-        if (localStorage.getItem(darkModeThemeStorageKey)) return localStorage.getItem(darkModeThemeStorageKey) == 'true'; // Return the dark mode preference from local storage
-        return (window.matchMedia && window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).matches); // Check if the user prefers dark mode
-    }
+	// Set the initial dark mode preference
+	setDarkMode(isDarkMode());
 
-    // Toggle the dark mode preference
-    function toggleDarkMode() {
-        setDarkMode(!isDarkMode());
-    }
-
-    // Listen for clicks on the toggle
-    document.getElementById(darkmodeToggleElementId).addEventListener('click', () => toggleDarkMode());
-
-    // Set the initial dark mode preference
-    setDarkMode(isDarkMode());
-
-    // Listen for changes in the user's preference
-    if (window.matchMedia) {
-        window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).addEventListener('change', event => {
-            const isDarkMode = event.matches;
-            setDarkMode(isDarkMode);
-        });
-    }
+	// Listen for changes in the user's preference
+	if (window.matchMedia) {
+		window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).addEventListener('change', event => {
+			const isDarkMode = event.matches;
+			setDarkMode(isDarkMode);
+		});
+	}
 });
